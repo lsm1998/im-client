d19ac3a678a915dc7538aa3c810fc8a1934de849
Administrator <487005831@qq.com>
2021-01-20 22:12:12 +0800

    文档

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 由Java语言完成的IM系统PC客户端
 
 编译环境基于jdk11（使用了一些高版本特性，降级为jdk8需要修改一些代码）
+
+[服务端](https://github.com/lsm1998/goim) 使用golang语言编写，基于gnet网络框架
--- src/test/java/com/lsm1998/im/SimpleTest.java
+++ src/test/java/com/lsm1998/im/SimpleTest.java
@@ -1,16 +1,36 @@
 package com.lsm1998.im;
 
-import com.lsm1998.im.utils.ImageUtil;
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
+import java.util.Arrays;
 
 public class SimpleTest
 {
+    int[] arr = {0, 1, 0, 3, 0};
+
     @Test
     public void test1()
     {
-        Image image = ImageUtil.getImageByUrl("http://git.strongberry.cn/uploads/-/system/user/avatar/1/avatar.png?width=40");
-        System.out.println(image);
+        int count = 0;
+        for (int i = 0; i < arr.length - count; i++)
+        {
+            if (arr[i] == 0)
+            {
+                swap(i, arr.length - count - 1);
+                count++;
+            }
+        }
+        System.out.println(Arrays.toString(this.arr));
+    }
+
+    private void swap(int i, int j)
+    {
+        if (this.arr[j] == 0)
+        {
+            swap(i,j - 1);
+        }
+        int temp = this.arr[i];
+        this.arr[i] = this.arr[j];
+        this.arr[j] = temp;
     }
 }
